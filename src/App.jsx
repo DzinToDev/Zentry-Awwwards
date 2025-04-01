c264c6a083ba8f88fb11ec4de4f95dcb3f88571e
DzinToDev <maheshdgaikwad18@gmail.com>
2025-04-01 23:38:56 +0530

    about us section done with animation wow

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Hero from "./components/Hero";
+import AboutUs from "./components/AboutUs";
 
 function App() {
   return (
     <main className=" relative min-h-screen w-screen overflow-x-hidden">
       <Hero />
-
-      <section className="z-0 min-h-screen bg-blue-400" />
+      <AboutUs />
+      <section className="w-screen h-dvh"></section>
     </main>
   );
 }
