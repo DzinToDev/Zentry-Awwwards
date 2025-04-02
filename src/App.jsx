ec18d558e8e0a0d38761a6094c1e9d3c4c2a1a85
DzinToDev <maheshdgaikwad18@gmail.com>
2025-04-02 13:58:20 +0530

    Navbar done with animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "./components/Hero";
 import AboutUs from "./components/AboutUs";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <main className=" relative min-h-screen w-screen overflow-x-hidden">
+      <Navbar />
       <Hero />
       <AboutUs />
       <section className="w-screen h-dvh"></section>
