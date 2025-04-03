413de19fe3b28b524f85870061cbe65e068581de
DzinToDev <maheshdgaikwad18@gmail.com>
2025-04-03 22:53:18 +0530

    footer hero text and tiltjs added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { FaDiscord, FaTwitter, FaYoutube, FaMedium } from "react-icons/fa";
+import Tilt from "react-parallax-tilt";
 
 const socialLinks = [
   { href: "https://discord.com", icon: <FaDiscord /> },
@@ -11,6 +12,14 @@ const socialLinks = [
 const Footer = () => {
   return (
     <footer className="bg-violet w-screen py-4 text-black">
+      <div className=" text-center transition-all duration-500 ease-in">
+        <Tilt>
+          <h1 className="special-font font-zentry-regular text-[34vw] transition-all duration-500 ease-in">
+            Zentry
+          </h1>
+        </Tilt>
+      </div>
+      
       <div className="container mx-auto flex flex-col items-center justify-between gap-4 px-4 md:flex-row">
         <p className="text-center text-sm font-light md:text-left">
           &copy; Zentry 2024. All rights reserved{" "}
