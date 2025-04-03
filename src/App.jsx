94298522657ebb21ef21979b9cac88950a520bf5
DzinToDev <maheshdgaikwad18@gmail.com>
2025-04-03 16:12:48 +0530

    feature section done with animation

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Hero from "./components/Hero";
 import AboutUs from "./components/AboutUs";
 import Navbar from "./components/Navbar";
+import Features from "./components/Features";
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
       <Navbar />
       <Hero />
       <AboutUs />
-      <section className="w-screen h-dvh"></section>
+      <Features />
     </main>
   );
 }
